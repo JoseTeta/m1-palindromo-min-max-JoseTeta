af9fb9f9d42d89bdb596ac5a20200f16f14fab79
João Vitor Castrechini <jvcastrequini@hotmail.com>
2024-12-02 10:49:07 -0300

    codigo-feito

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 function isPalindrome(str){
-    /* Seu código aqui */
-}
+    
+    
+        let cleanedStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+        
+        
+        let length = cleanedStr.length;
+      
+        
+        for (let i = 0; i < Math.floor(length / 2); i++) {
+          if (cleanedStr[i] !== cleanedStr[length - 1 - i]) {
+            return false; 
+          }
+        }
+      
+        return true; 
+      }
+          
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+    let min = arr[0];
+        let max = arr[0];
+        
+        
+        let i = 1;
+      
+        
+        while (i < arr.length) {
+          if (arr[i] < min) {
+            min = arr[i]; 
+          }
+          if (arr[i] > max) {
+            max = arr[i]; 
+          }
+          i++; 
+        }
+        
+        
+        return [min, max];
+      }
